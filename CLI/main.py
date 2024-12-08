a3e92c554e6b2d7f36933391280bbae70c3c1096
paw pour de vrai <142060742+pewpewpaw@users.noreply.github.com>
2024-12-08 22:44:21 +0100

    Update main.py
    corrections

--- CLI/main.py
+++ CLI/main.py
@@ -4,50 +4,43 @@ from colorama import *
 
 init(autoreset=True)
 
-get_latest_cves()
-
-print("""
-      
-      hello to the newsletter automatisation ( check the readme page to see how to use it)
-      
-      
-      1. See the news about cyber (single url).
-      2. Search for CVE.
-      3. 10 latest news.
-      4. Enable the long time mode.
-      5. scan for vulnerabilities.
-      
-      """)
 
 while True:
+    display_menu()
+    
     choice = input("what's do you want to do ? : ")
 
     if choice == "1":
-        news_single()
+        domain = input("domain/ip :")
+        scan(domain)
         
             
     elif choice == "2":
-        adv = input("Do you wan't to search about cve with her details or scan about service version ? (CVE/SERV)")
-        if adv.lower() == "cve":
+        adv = input("Do you wan't to search about cve with her details ? (Y/N)")
+        if adv.lower() == "y":
             cve_id = input("aliases of the cve (format : CVE-XXXX-XXXX ): ")
-            cve_details(cve_id)
+            print(Back.RED + "Warning : the description of cve use the 3.1 version of cvss for better, more reliable result.")
+            cve_details(cve_id.upper())
         
-        elif adv.lower() == "serv":
-            service = input("service : ")
-            version = input ("version : ")
-            cve_simple(service, version)
+        elif adv.lower() == "n":
+            cve_id = input("aliases of the cve (format : CVE-XXXX-XXXX ): ")
+            cve_simple(cve_id.upper())
 
         else:
-            print("please choose beetwen cve or serv ")   
+            print("please choose beetwen yes or no ")   
 
     elif choice == "3":
         top_article()
 
     elif choice == "4":
-        long_time()
-        
+        pass
+    
     elif choice == "5":
-        scan_vuln()
+        services = input("service : ")
+        version = input("version : ")
+        scan_serv(services, version)
+        
+        
     
     else:
-        print("choisit entre 1 et 5")
+        print("choice beetewen 1 and 5")
